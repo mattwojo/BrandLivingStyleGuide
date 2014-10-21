5c1f5c02504b6eea6df8e39b735c7aaaa60b8cfe
mattwojo <mattwojciakowski@gmail.com>
2014-10-21 14:39:46 -0700

    Node Express Project Set Up

---
+++ BrandLivingStyleGuide/gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    sass: {
+      dist: {
+        files: {
+          'public/stylesheets/application.css': 'sass/application.scss'
+        },
+        options: {
+          sourceMap: true,
+          includePaths: [
+            require('node-bourbon').includePaths,
+            './bower_components/color-scale',
+          ]
+        }
+      }
+    },
+    watch: {
+      source: {
+        files: ['sass/**/*.scss', 'views/**/*.jade'],
+        tasks: ['sass'],
+        options: {
+          livereload: true, // needed to run LiveReload
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.registerTask('default', ['sass']);
+};
