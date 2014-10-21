22371389a2caa440e410c834d59dc90265f23b9f
Maggie Walker <magwalk@gmail.com>
2014-10-21 15:52:29 -0700

    ROUTES FOR THE WIN

--- BrandLivingStyleGuide/app.js
+++ BrandLivingStyleGuide/app.js
@@ -5,15 +5,30 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var router = express.Router();
 
-var routes = require('./routes');
-var users = require('./routes/user');
+var routes = require('./routes/index');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+app.get('/', routes.index);
+
+
+app.get('/', function(req,res) {
+    res.render('index.jade', {title: 'index'});
+});
+
+app.get('/buttons', function(req,res) {
+    res.render('buttons.jade', {title: 'buttons'});
+});
+
+app.get('/grid', function(req,res) {
+    res.render('grid.jade', {title: 'grid'});
+});
 
 app.use(favicon());
 app.use(logger('dev'));
@@ -22,9 +37,9 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(app.router);
+app.use('/', routes);
 
-app.get('/', routes.index);
-app.get('/users', users.list);
+
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
@@ -46,6 +61,8 @@ if (app.get('env') === 'development') {
     });
 }
 
+
+
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
@@ -54,6 +71,7 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
+
 
 
 module.exports = app;
