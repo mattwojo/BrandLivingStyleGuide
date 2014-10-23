5b902c5333325ca9bdcb7115bce60b3969bf3d88
mattwojo <mattwojciakowski@gmail.com>
2014-10-23 16:28:45 -0700

    Added Code Notes for Bar Charts

--- BrandLivingStyleGuide/gruntfile.js
+++ BrandLivingStyleGuide/gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt) {
           includePaths: [
             //require('node-bourbon').includePaths,
             './bower_components/color-scale',
-
+          ]
         }
       }
     },
